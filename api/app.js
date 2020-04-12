81e31dceaa84200ee1d5d108bc113a3fdc879c51
Eleanor McMurtry <elmcmurtry1@gmail.com>
2020-04-12 19:01:11 +1000

    fix security vulns by stripping down packages

--- api/app.js
+++ api/app.js
@@ -5,15 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const cors = require('cors');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-const testApiRouter = require('./routes/testApi');
-
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(cors());
 app.use(logger('dev'));
@@ -22,9 +14,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/api', testApiRouter);
+const apiRouter = require('./routes/api');
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
